f8b94af3122ab03804f107a256700af7eb900d15
ryohey <info@codingcafe.jp>
2023-06-05 11:09:19 +0900

    Fix authorization method

--- browser.js
+++ browser.js
@@ -1,80 +1,128 @@
 import keytar from "keytar"
 import promptly from "promptly"
+import { select } from "@inquirer/prompts"
+import assert from "assert"
 import { launch } from "puppeteer"
 
-const service = "money-forward-attendance-cli"
+const serviceMoneyForwardID = "money-forward-attendance-cli (MoneyForward ID)"
+const serviceMoneyForwardCompanyID =
+  "money-forward-attendance-cli (MoneyForward Company ID)"
 
-/*
- keytar には account/password のペアで保存するので、
- office と email を結合した文字列を account として扱う
- */
-function joinAccount(office, email) {
-  return [office, email].join("\t")
-}
-
-function splitAccount(account) {
-  const c = account.split("\t")
-  return { office: c[0], email: c[1] }
-}
-
-async function getCredentials() {
-  const credentials = await keytar.findCredentials(service)
+async function getMFIDCredentials() {
+  const credentials = await keytar.findCredentials(serviceMoneyForwardID)
 
   if (credentials.length > 0) {
     const cred = credentials[0]
-    console.log("Use saved password from the keychain")
-    return {
-      ...splitAccount(cred.account),
-      password: cred.password,
-    }
+    console.log("Use saved password for MoneyForward ID from the keychain")
+    return { email: cred.account, password: cred.password }
   }
 
-  const email = await promptly.prompt("Type your email or account name: ")
-  const office = await promptly.prompt("Type company account name: ")
-  const password = await promptly.password("Type your password: ")
-  const account = joinAccount(office, email)
+  console.log("Input your MoneyForward ID credentials")
 
-  keytar.setPassword(service, account, password)
+  const account = await promptly.prompt("Type your email: ")
+  assert(account.length > 0, "Invalid email")
+
+  const password = await promptly.password("Type your password: ")
+  assert(password.length > 0, "Invalid password")
+
+  keytar.setPassword(serviceMoneyForwardID, account, password)
   console.log("Password saved in the keychain")
 
-  return { office, email, password }
+  return { email: account, password }
 }
 
-async function getAuthorizedPage(browser) {
-  const { office, email, password } = await getCredentials()
-
-  console.log("Try authorization")
-  const page = await browser.newPage()
+async function authorizeMoneyForwardID(page) {
+  const { email, password } = await getMFIDCredentials()
 
   await page.goto("https://attendance.moneyforward.com/employee_session/new")
 
-  await page.setViewport({ width: 1412, height: 941 })
+  await page.waitForSelector(".attendance-button-mfid")
+  await page.click(".attendance-button-mfid")
 
-  await page.waitForSelector("#employee_session_form_office_account_name")
-  await page.type("#employee_session_form_office_account_name", office)
+  // https://id.moneyforward.com/sign_in/email へリダイレクトされる
+  // MoneyForward ID でログインする
 
-  await page.waitForSelector("#employee_session_form_account_name_or_email")
-  await page.type("#employee_session_form_account_name_or_email", email)
+  // メールアドレス入力
+  await page.waitForSelector('input[type="email"]')
+  await page.type('input[type="email"]', email)
 
-  await page.waitForSelector("#employee_session_form_password")
-  await page.type("#employee_session_form_password", password)
-
-  await page.waitForSelector(
-    ".attendance-before-login-card-contents > .attendance-before-login-card-button > form > .attendance-before-login-card-button > .attendance-button-email"
-  )
-  await page.click(
-    ".attendance-before-login-card-contents > .attendance-before-login-card-button > form > .attendance-before-login-card-button > .attendance-button-email"
-  )
+  // 送信ボタンクリック
+  await page.waitForSelector("input[type=submit]")
+  await page.click("input[type=submit]")
 
   await page.waitForNavigation()
+
+  // パスワード入力
+  await page.waitForSelector("input[type=password]")
+  await page.type("input[type=password]", password)
+
+  // 送信ボタンクリック
+  await page.waitForSelector("input[type=submit]")
+  await page.click("input[type=submit]")
+
+  await page.waitForNavigation()
+
+  // 事業者選択
+  const companies = await page.evaluate(() =>
+    Array.from(document.querySelectorAll(".attendance-table-contents tr"))
+      .map((e) => Array.from(e.querySelectorAll("td")))
+      .filter((tr) => tr.length === 3)
+      .map((tr) => ({
+        id: tr[0].textContent,
+        name: tr[1].textContent,
+        link: tr[2].querySelector(".attendance-button-primary").attributes.href
+          .value,
+      }))
+      .map((c) => ({
+        name: `${c.name} (${c.id})`,
+        value: c.link,
+      }))
+  )
+
+  console.dir(companies)
+
+  let selectedCompanyLink = await keytar.findPassword(
+    serviceMoneyForwardCompanyID,
+    email
+  )
+
+  if (selectedCompanyLink === null) {
+    selectedCompanyLink = await select({
+      message: "Choose your company",
+      choices: companies,
+    })
+    await keytar.setPassword(
+      serviceMoneyForwardCompanyID,
+      email,
+      selectedCompanyLink
+    )
+    console.log("Company saved in the keychain")
+  } else {
+    console.log("Use saved company from the keychain")
+  }
+
+  await page.click(`a[href='${selectedCompanyLink}']`)
+
+  await page.waitForNavigation()
+
   console.log("Authorization successful")
+}
+
+async function getAuthorizedPage(browser) {
+  console.log("Try authorization")
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1412, height: 941 })
+
+  await authorizeMoneyForwardID(page)
 
   return page
 }
 
 export async function clockIn() {
-  const browser = await launch()
+  const browser = await launch({ headless: false })
   const page = await getAuthorizedPage(browser)
+
+  await page.goto("https://attendance.moneyforward.com/my_page")
 
   await page.waitForSelector(
     ".tw-flex > .tw-relative > .button-list > .clock_in > .time-stamp-button"
@@ -83,7 +131,12 @@ export async function clockIn() {
     ".tw-flex > .tw-relative > .button-list > .clock_in > .time-stamp-button"
   )
 
-  await page.waitForNavigation()
+  await page.waitForSelector(".action-message.success")
+  const message = await page.evaluate(() =>
+    document.querySelector(".action-message.success").textContent.trim()
+  )
+  console.log(message)
+
   await browser.close()
   console.log("Clock-in success")
 }
@@ -92,6 +145,8 @@ export async function clockOut() {
   const browser = await launch()
   const page = await getAuthorizedPage(browser)
 
+  await page.goto("https://attendance.moneyforward.com/my_page")
+
   await page.waitForSelector(
     ".tw-flex > .tw-relative > .button-list > .clock_out > .time-stamp-button"
   )
@@ -99,7 +154,12 @@ export async function clockOut() {
     ".tw-flex > .tw-relative > .button-list > .clock_out > .time-stamp-button"
   )
 
-  await page.waitForNavigation()
+  await page.waitForSelector(".action-message.success")
+  const message = await page.evaluate(() =>
+    document.querySelector(".action-message.success").textContent.trim()
+  )
+  console.log(message)
+
   await browser.close()
   console.log("Clock-out success")
 }
